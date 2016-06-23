96091e36b2d5de7219150a0d59cf48019af29e9b
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2016-06-23 09:49:31 +0200

    Generate sieve and sieve-debug executables

--- c/nbproject/Makefile-Debug.mk
+++ c/nbproject/Makefile-Debug.mk
@@ -52,15 +52,14 @@ FFLAGS=
 ASFLAGS=
 
 # Link Libraries and Options
-LDLIBSOPTIONS=-lm
+LDLIBSOPTIONS=-Wl,-rpath,'.' -lm
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
-	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/c
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk sieve-debug
 
-${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/c: ${OBJECTFILES}
-	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
-	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/c ${OBJECTFILES} ${LDLIBSOPTIONS}
+sieve-debug: ${OBJECTFILES}
+	${LINK.c} -o sieve-debug ${OBJECTFILES} ${LDLIBSOPTIONS}
 
 ${OBJECTDIR}/main.o: main.c 
 	${MKDIR} -p ${OBJECTDIR}
@@ -73,7 +72,7 @@ ${OBJECTDIR}/main.o: main.c
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
 	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
-	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/c
+	${RM} -r null
 
 # Subprojects
 .clean-subprojects:
--- c/nbproject/Makefile-variables.mk
+++ c/nbproject/Makefile-variables.mk
@@ -8,17 +8,17 @@ CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=GNU-Linux
-CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
-CND_ARTIFACT_NAME_Debug=c
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/c
+CND_ARTIFACT_DIR_Debug=
+CND_ARTIFACT_NAME_Debug=sieve-debug
+CND_ARTIFACT_PATH_Debug=sieve-debug
 CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
 CND_PACKAGE_NAME_Debug=c.tar
 CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/c.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux
-CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
-CND_ARTIFACT_NAME_Release=c
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/c
+CND_ARTIFACT_DIR_Release=
+CND_ARTIFACT_NAME_Release=sieve
+CND_ARTIFACT_PATH_Release=sieve
 CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
 CND_PACKAGE_NAME_Release=c.tar
 CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/c.tar
