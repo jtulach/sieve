89cbfba97e098c4e595a5bf891f0979edeea92be
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2017-06-08 10:14:02 +0300

    Separate the directory traversal logic into its own method

--- ruby+js/fromjava/src/main/java/org/apidesign/demo/rubyjs/fromjava/Main.java
+++ ruby+js/fromjava/src/main/java/org/apidesign/demo/rubyjs/fromjava/Main.java
@@ -3,6 +3,7 @@ package org.apidesign.demo.rubyjs.fromjava;
 import com.oracle.truffle.api.source.Source;
 import com.oracle.truffle.api.vm.PolyglotEngine;
 import java.io.File;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 public final class Main {
@@ -14,6 +15,15 @@ public final class Main {
         PolyglotEngine vm = PolyglotEngine.newBuilder().
             build();
 
+        File scriptDir = findScriptDir();
+        Source ruby = Source.newBuilder(new File(scriptDir, "sieve.rb")).build();
+        Source js = Source.newBuilder(new File(scriptDir, "sieve.js")).build();
+
+        vm.eval(ruby);
+        vm.eval(js);
+    }
+
+    private static File findScriptDir() throws URISyntaxException {
         URL url = Main.class.getProtectionDomain().getCodeSource().getLocation();
         File local = new File(url.toURI());
         for (;;) {
@@ -23,11 +33,6 @@ public final class Main {
             }
             local = local.getParentFile();
         }
-
-        Source ruby = Source.newBuilder(new File(local, "sieve.rb")).build();
-        Source js = Source.newBuilder(new File(local, "sieve.js")).build();
-
-        vm.eval(ruby);
-        vm.eval(js);
+        return local;
     }
 }
