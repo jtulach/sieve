05df0f2ca6f7aa33ecba22201c534840c7b7c48b
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2018-10-26 08:22:45 +0200

    Use short component names when installing graal components

--- .test.sh
+++ .test.sh
@@ -9,6 +9,17 @@ fi
 
 GRAALBIN=$1/bin
 
+function ginstall() {
+  if [ -e $GRAALBIN/gu ]; then
+    if $GRAALBIN/gu list | grep "^$1 "; then
+      echo $1 is installed
+    else
+      $GRAALBIN/gu install $1
+      REBUILD=true
+    fi
+  fi
+}
+
 make -C c
 ./c/sieve 25
 
@@ -22,22 +33,11 @@ if [ "x" != "x$DISPLAY" ]; then
     mvn -q -f java/client/pom.xml exec:exec -Drepeat=5
 fi
 
-function install() {
-  if [ -e $GRAALBIN/gu ]; then
-    if $GRAALBIN/gu list | grep $1; then
-      echo $1 is installed
-    else
-      $GRAALBIN/gu install $1
-      REBUILD=true
-    fi
-  fi
-}
-
-install org.graalvm.R
+ginstall R
 
 $GRAALBIN/polyglot --jvm --eval "js:count=25" --file R+js/sieve.R --file R+js/sieve.js
 
-install org.graalvm.ruby
+ginstall ruby
 
 JAVA_HOME=$1 mvn -q clean package exec:exec -f ruby+js/fromjava/pom.xml -Drepeat=25
 $GRAALBIN/ruby ruby/sieve.rb 25
@@ -46,7 +46,7 @@ $GRAALBIN/node js/sieve.js 15
 mvn -q -f js/fromjava/pom.xml package exec:exec -Drepeat=15
 JAVA_HOME=$1 mvn -q -f js/fromjava/pom.xml package exec:exec -Drepeat=15
 
-install org.graalvm.python
+ginstall python
 
 $GRAALBIN/graalpython python/sieve.py 15
 
