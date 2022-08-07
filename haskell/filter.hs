ada484798c0cef5abe7078e8a0d67ee2b1258efe
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2022-08-07 18:42:11 +0200

    Getting ready for an appendable filter by implementing finger tree

---
+++ haskell/filter.hs
@@ -0,0 +1,53 @@
+
+
+data FingerTree a =
+    Empty |
+    Single a |
+    Deep (Digit a) (FingerTree (Node a)) (Digit a)
+    deriving (Show)
+
+data Digit a =
+    One a |
+    Two a a |
+    Three a a a |
+    Four a a a a
+    deriving (Show)
+
+fromdigit :: Digit a -> [a]
+fromdigit (One x) = [x]
+fromdigit (Two x y) = [x, y]
+fromdigit (Three x y z) = [x, y, z]
+fromdigit (Four x y z v) = [x, y, z, v]
+
+data Node a =
+    Node2 a a |
+    Node3 a a a
+    deriving (Show)
+
+fromnode :: Node a -> [a]
+fromnode (Node2 x y) = [x, y]
+fromnode (Node3 x y z) = [x, y, z]
+
+
+append :: a -> FingerTree a -> FingerTree a
+append x Empty = Single x
+append x (Single e) = Deep (One e) Empty (One x)
+append x (Deep p t a) = case a of
+    One e -> Deep p t (Two e x)
+    Two e1 e2 -> Deep p t (Three e1 e2 x)
+    Three e1 e2 e3 -> Deep p t (Four e1 e2 e3 x)
+    Four e1 e2 e3 e4 -> Deep p (append (Node3 e1 e2 e3) t) (Two e4 x)
+
+aslist :: FingerTree a -> [a]
+aslist Empty = []
+aslist (Single e) = [e]
+aslist (Deep p t a) = (fromdigit p) ++ (listofa t) ++ (fromdigit a)
+    where
+        listofnodes :: FingerTree (Node a) -> [Node a]
+        listofnodes t = aslist t
+
+        listoflista :: FingerTree (Node a) -> [[a]]
+        listoflista t = map fromnode (listofnodes t)
+
+        listofa :: FingerTree (Node a) -> [a]
+        listofa t = foldr (++) [] (listoflista t)
